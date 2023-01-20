8b3819729aed12671cd3c54e7a3a2a957792352c
Ali Yousefi <ali.visual.studio@gmail.com>
2023-01-20 10:38:26 +0330

    Add support for EF Core provider. #2

---
+++ src/CSharp/EasyMicroservices.Database/EasyMicroservices.Database.EntityFrameworkCore/Providers/EntityframeworkCoreDatabaseProvider.cs
@@ -0,0 +1,40 @@
+﻿using EasyMicroservices.Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.Database.EntityFrameworkCore.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EntityframeworkCoreDatabaseProvider : IDatabase, IAsyncDisposable
+    {
+        private readonly DbContext _dbContext;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dbContext"></param>
+        public EntityframeworkCoreDatabaseProvider(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public IEasyQueryableAsync<TEntity> GetQueryOf<TEntity>() where TEntity : class
+        {
+            return new EntityframeworkCoreQueryableProvider<TEntity>(_dbContext.Set<TEntity>());
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ValueTask DisposeAsync()
+        {
+            return _dbContext.DisposeAsync();
+        }
+    }
+}
