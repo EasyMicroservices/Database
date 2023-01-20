835fc4b83a2be4f9a474231394253872d945c321
Ali Yousefi <ali.visual.studio@gmail.com>
2023-01-20 10:38:08 +0330

    Create base structure of database management #1

---
+++ src/CSharp/EasyMicroservices.Database/EasyMicroservices.Database/Interfaces/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyMicroservices.Database.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDatabase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public IEasyQueryableAsync<TEntity> GetQueryOf<TEntity>()
+            where TEntity : class;
+    }
+}
