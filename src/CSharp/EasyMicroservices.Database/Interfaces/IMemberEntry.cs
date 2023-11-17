e8a2ddf94afef12bd6986cf668bf087a748f18df
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-17 12:34:44 +0330

    Add Support for PropertyEntry

---
+++ src/CSharp/EasyMicroservices.Database/Interfaces/IMemberEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyMicroservices.Database.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMemberEntry
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsModified { get; set; }
+    }
+}
